34c860f6fa801ce61b1dd08aa3a012df54c3c2a5
Vanessa Villagomez <villagomez.v@outlook.com>
2021-11-28 21:36:06 -0800

    update schema sql file

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,37 +1,60 @@
-﻿-- Create the titles table, with a primary id and a title
+﻿-- Drop table if exists
+DROP TABLE IF EXISTS titles;
+-- Create the titles table, with a primary id and a title
+CREATE TABLE titles (
+	title_id INT PRIMARY KEY,
+	title VARCHAR
+);
 
-
+-- Drop table if exists
+DROP TABLE IF EXISTS employees;
 -- Create an employees table, with the following: 
---    employee number, 
---    employee title id,
---    birth date
---    first name
---    last name
---    sex
---    hire date
 -- set the foreign key and primary key relationships accordingly
+CREATE TABLE employees (
+	 emp_no INT PRIMARY KEY, 
+	 emp_title_id VARCHAR FOREIGN KEY REFERENCES titles(title_id),
+	 birth_date DATE,
+	 first_name VARCHAR,
+	 last_name VARCHAR,
+	 sex VARCHAR,
+	 hire_date DATE
+);
 
-
+-- Drop table if exists
+DROP TABLE IF EXISTS departments;
 -- Create an departments table, with the following: 
---    department number, 
---    department name
 -- set the primary key relationships accordingly
+CREATE TABLE departments (
+	dept_no	VARCHAR PRIMARY KEY, 
+	dept_name VARCHAR
+);
 
-
+-- Drop table if exists
+DROP TABLE IF EXISTS dept_managers;
 -- Create an dept_manager table, with the following: 
---    department number, 
---    employee number
 -- set the foreign key and primary key relationships accordingly
+CREATE TABLE dept_managers (
+	dept_no VARCHAR FOREIGN KEY REFERENCES departments(dept_no),
+	emp_no INT FOREIGN KEY REFERENCES employees(emp_no)
+);
 
-
+-- Drop table if exists
+DROP TABLE IF EXISTS dept_emp;
 -- Create an dept_emp table, with the following: 
---    department number, 
---    employee number
 -- set the foreign key and primary key relationships accordingly
+CREATE TABLE dept_emp (
+	dept_no VARCHAR FOREIGN KEY REFERENCES departments(dept_no), 
+	emp_no INT FOREIGN KEY REFERENCES employees(emp_no)
+);
 
-
+-- Drop table if exists
+DROP TABLE IF EXISTS salaries;
 -- Create an salaries table, with the following: 
---    salary 
---    employee number
 -- set the foreign key and primary key relationships accordingly
+CREATE TABLE salaries (
+	salary INT,
+	emp_no INT FOREIGN KEY REFERENCES employees(emp_no)
+);
+
+
 
