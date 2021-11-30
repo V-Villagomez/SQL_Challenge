6666e031e31ad84b5219bff4f52c79b7da538719
Vanessa Villagomez <villagomez.v@outlook.com>
2021-11-30 00:42:05 -0800

    add new file

---
+++ EmployeeSQL/epilogue.sql
@@ -0,0 +1,7 @@
+SELECT
+	e.first_name AS "First Name",
+	e.last_name AS "Last Name"
+FROM employees e
+WHERE e.emp_no = 499942;
+
+-- First Name and Last Name for employee number 499942 is April Foolsday!
