f844a0552d1703a6cad90c77bb81e073436f567f
Vanessa Villagomez <villagomez.v@outlook.com>
2021-11-29 19:12:47 -0800

    update file

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,34 +1,37 @@
 ﻿-- Drop table if exists
-DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS titles CASCADE;
 -- Create the titles table, with a primary id and a title
 CREATE TABLE titles (
-	title_id VARCHAR PRIMARY KEY,
-	title VARCHAR
-);
+	title_id VARCHAR,
+	title VARCHAR,
+	CONSTRAINT pk_titles PRIMARY KEY (title_id)
+); 
 
 -- Drop table if exists
-DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS employees CASCADE;
 -- Create an employees table, with the following: 
 -- set the foreign key and primary key relationships accordingly
 CREATE TABLE employees (
-	 emp_no INT PRIMARY KEY,
+	 emp_no INT,
 	 emp_title_id VARCHAR,
-	 --FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	 birth_date DATE,
 	 first_name VARCHAR,
 	 last_name VARCHAR,
 	 sex VARCHAR,
-	 hire_date DATE
+	 hire_date DATE,
+	 FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+	 CONSTRAINT pk_employees PRIMARY KEY (emp_no)
 	 --md5(concat(emp_no, emp_title_id, birth_date, first_name, last_name, sex, hire_date))
 );
 
 -- Drop table if exists
-DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS departments CASCADE;
 -- Create an departments table, with the following: 
 -- set the primary key relationships accordingly
 CREATE TABLE departments (
-	dept_no	VARCHAR PRIMARY KEY, 
-	dept_name VARCHAR
+	dept_no	VARCHAR, 
+	dept_name VARCHAR,
+	CONSTRAINT pk_departments PRIMARY KEY (dept_no)
 );
 
 -- Drop table if exists
@@ -37,9 +40,9 @@ DROP TABLE IF EXISTS dept_managers;
 -- set the foreign key and primary key relationships accordingly
 CREATE TABLE dept_managers (
 	dept_no VARCHAR, 
-	--FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	emp_no INT 
-	--FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	emp_no INT,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 -- Drop table if exists
@@ -47,10 +50,10 @@ DROP TABLE IF EXISTS dept_emp;
 -- Create an dept_emp table, with the following: 
 -- set the foreign key and primary key relationships accordingly
 CREATE TABLE dept_emp (
-	dept_no VARCHAR, 
-	--FOREIGN KEY (dept_no) REFERENCES departments(dept_no), 
-	emp_no INT 
-	--FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	dept_no VARCHAR,
+	emp_no INT,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 -- Drop table if exists
@@ -59,25 +62,7 @@ DROP TABLE IF EXISTS salaries;
 -- set the foreign key and primary key relationships accordingly
 CREATE TABLE salaries (
 	salary INT,
-	emp_no INT 
-	--FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	emp_no INT, 
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
--- Create alter tables to set foreign key relationships accordingly
-ALTER TABLE employees
-ADD FOREIGN KEY (emp_title_id) REFERENCES titles(title_id);
-
-ALTER TABLE dept_managers
-ADD FOREIGN KEY (dept_no) REFERENCES departments(dept_no);
-	
-ALTER TABLE dept_managers
-ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
-
-ALTER TABLE dept_emp
-ADD FOREIGN KEY (dept_no) REFERENCES departments(dept_no);
-
-ALTER TABLE dept_emp 
-ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
-
-ALTER TABLE salaries
-ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
